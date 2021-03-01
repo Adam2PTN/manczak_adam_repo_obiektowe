c85934540c570fc37531d222bbed58425b5ad5b2
Adam2PTN <adas.manczak@wp.pl>
2021-03-01 09:29:52 +0100

    Przeniesienie funkcjonalności do metody tab2dFirst i przeniesienie do przypomnienieTablicJednowymiarowych

--- Tablica_dwuwymiarowa/TablicaDwuwymiarowa.java
+++ Tablica_dwuwymiarowa/TablicaDwuwymiarowa.java
@@ -1,10 +1,26 @@
 class TablicaDwuwymiarowa{
 	public static void main(String [] args){
+		
+	}
+	private static void tab2dFirst(){
+
+		int [][] tab2d = new int[5][2];
+		tab2d[0][2] = 50;
+		tab2d[2][1] = -200;
+		tab2d[4][0] = 444;
+		for (int i=0;i<tab2d.length;i++){
+			for (int j=0;i<tab2d[j].length;j++){
+				System.out.println(tab2d[i][j]);
+			}
+			System.out.println("Koniec pętli ze zmienną i");
+		}
+		System.out.println("Koniec pętli ze zmienną i");
+	}
+}
+	private static void przupomnienieTablicJenowymiarowych{
 		int[] tab = new int[5];
 		tab[0] = 40;
 		for(int i=0;i<tab.length;i++){
 			System.out.println(tab[i]);
 		}
-		int [][] tab2d = new int[5][2];
 	}
-}
