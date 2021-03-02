9a28ad7519e3f9107eb5e4f8dc4ce99f85c7f696
Adam2PTN <adas.manczak@wp.pl>
2021-03-02 10:05:41 +0100

    Wykonanie działających tablic

--- 02.03.2021/TablicaDwuwymiarowa2.java
+++ 02.03.2021/TablicaDwuwymiarowa2.java
@@ -4,7 +4,19 @@ class TablicaDwuwymiarowa2{
 	}
 	
 	private static void tablica2x2(){
-	//funkcje
+		int [][] tab = new int[5][2];
+		tab[0][0] = 0;
+		tab[1][0] = 1;
+		tab[2][0] = 2;
+		tab[3][0] = 3;
+		for (int i=0;i<tab.length;i++){
+			for (int j=0;i<tab[j].length;j++){
+				System.out.println(tab[i][j]);
+			}
+			System.out.println("Koniec pętli ze zmienną i");
+		}
+		System.out.println("Koniec pętli ze zmienną i");
+	}
 	}
 	
 	
