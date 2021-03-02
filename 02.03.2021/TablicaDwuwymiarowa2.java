e21a35574a7e6772936b212de33de2f29181b264
Adam2PTN <adas.manczak@wp.pl>
2021-03-02 10:30:52 +0100

    tablicaTabliczkaMnozenia

--- 02.03.2021/TablicaDwuwymiarowa2.java
+++ 02.03.2021/TablicaDwuwymiarowa2.java
@@ -3,6 +3,21 @@ class TablicaDwuwymiarowa2{
 		
 	}
 	
+	private static void tablicaTabliczkaMnozenia(){
+		int [][] tab = new int[10][10];
+		for (int i=0;i<tab.length;i++){
+			for (int j=0;i<tab[j].length;j++){
+				tab[i][j] = (i+1)*(j+1);
+			}
+		}
+		System.out.println("Tabliczka mnozenia");
+		for (int i=0;i<tab.length;i++){
+			for (int j=0;i<tab[j].length;j++){
+				System.out.println(tab[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
 	private static void tablica2x2(){
 		int [][] tab = new int[5][2];
 		tab[0][0] = 0;
