c7c5af3d1ee7eb902c5222b3ca9deac2626f1c64
Adam2PTN <adas.manczak@wp.pl>
2021-04-13 10:23:45 +0200

    Obliczanie drugiego pola trojkata

--- Trojkat/Main.java
+++ Trojkat/Main.java
@@ -1,5 +1,6 @@
 class Main{
 	public static void main(String [] args){
 		Trojkat t1 = new Trojkat(20,3);
+		Trojkat t2 = new Trojkat(6,8,3,3);
 	}
 }
--- Trojkat/Trojkat.java
+++ Trojkat/Trojkat.java
@@ -1,7 +1,8 @@
 class Trojkat{
-	public int a = 0;
-	public int h = 0;
 	public Trojkat(int a, int h){
 		System.out.println("Podstawa: "+a+"\nWysokosc: "+h+"\nPole trojkata: "+a*h/2);
+	}
+	public Trojkat(int a, int b, int c, int r){
+		System.out.println("\nDlugosc boku: "+a+"\nDlugosc boku: "+b+"\nDlugosc boku: "+c+"\nPromien okregu: "+r+"\nPole trojkata: "+a*b*c/4*r);
 	}
 }
