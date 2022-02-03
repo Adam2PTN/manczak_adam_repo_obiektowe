48965c71933146899eafc6246337cc70f73f7f30
Adam2PTN <77607603+Adam2PTN@users.noreply.github.com>
2022-02-03 11:27:35 +0100

    sprawdzenie1

---
+++ Projekt/Card.java
@@ -0,0 +1,7 @@
+class Card{
+    private int rank;
+    private int suit;
+    private int value;
+    private static String[] ranks = {"Jack","Joker","Ace","King","Queen","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+    private static String[] suits = {"Clubs","Diamonds","Hearts","Spades"};
+}
--- Projekt/Main.java
+++ Projekt/Main.java
@@ -1,3 +1,9 @@
-class Main{
-
+public class Main{
+    public static void main(String[] args) {
+        private static String name;
+        private static int cash;
+        private static int bet;
+        private static int aceCounter;
+        private static int handvalue;
+    }
 }
