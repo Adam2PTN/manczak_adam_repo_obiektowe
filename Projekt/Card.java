b7ea757111d2c1ea3e8fa83b947133bbf86de205
Adam2PTN <77607603+Adam2PTN@users.noreply.github.com>
2022-02-03 12:13:45 +0100

    drugi kontrolny

--- Projekt/Card.java
+++ Projekt/Card.java
@@ -2,6 +2,33 @@ class Card{
     private int rank;
     private int suit;
     private int value;
-    private static String[] ranks = {"Jack","Joker","Ace","King","Queen","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+    private static String[] ranks = {"Jack","Ace","King","Queen","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
     private static String[] suits = {"Clubs","Diamonds","Hearts","Spades"};
+
+    public Card(int suit, int values){
+        this.rank=values;
+        this.suit=suit;
+    }
+    public String toString(){
+        return ranks[rank]+" "+suits[suit];
+    }
+    public int getRank(){
+        return rank;
+    }
+    public int getValue(){
+        if(rank>10){
+            value=10;
+        }
+        else if(rank==1){
+            value=11;
+        }
+        else{
+            value=rank;
+        }
+        return value;
+        }
+        public void setValue(int set)
+        {
+            value = set;
+        }
 }
--- Projekt/Main.java
+++ Projekt/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Main{
-    public static void main(String[] args) {
-        private static String name;
-        private static int cash;
-        private static int bet;
-        private static int aceCounter;
-        private static int handvalue;
+    private static String name;
+    private static int cash;
+    private static int bet;
+    private static int aceCounter;
+    private static int handvalue;
+    
+    public static void main(String[] args){
+        System.out.println("Podaj swoje imie: ");
+        Scanner namescan = new Scanner(System.in);
+        name = namescan.nextLine();
+        System.out.println(name);
+        System.out.println("Z jaka waluta chcesz rozpoczac gre?");
+        Scanner money = new Scanner(System.in);
+        cash = money.nextInt();
+        System.out.println(cash);
     }
 }
